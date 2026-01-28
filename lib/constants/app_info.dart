6dbf829f32fb0c635867641b8734bd793d23a317
Freedom Guard Net <27111572-Freedom-Guard-Net@users.noreply.gitlab.com>
2026-01-28 19:38:40 +0000

    UPDATE 12: FIX 3

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,5 +1,5 @@
 class AppInfo {
-  static const String version = '12.5.1';
+  static const String version = '12.0.1';
   static const int buildVersion = 12;
   static const String appName = 'Freedom Guard';
   static const String appDescription = '';
