0815897685666f6ed2801b5f96462123efd0c997
FG Builder <FreedomGuard@duck.com>
2026-02-15 18:13:31 +0330

    UPDATE 12.0.7: STB

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,5 +1,5 @@
 class AppInfo {
-  static const String version = '12.0.6';
+  static const String version = '12.0.7';
   static const int buildVersion = 12;
   static const String appName = 'Freedom Guard';
   static const String appDescription = '';
